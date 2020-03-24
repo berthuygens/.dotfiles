961b1eed408a0691d9f7002a94c344ddeb0f3f70
berthuygens <bert.huygens@inbo.be>
2020-03-24 13:22:53 +0100

    created more symlinks

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitignore_global ~/gitignore_global
+ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/bash/bash_profile ~/.bashprofile
+ln -s ~/.dotfiles/vim/.vim ~/
+ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
